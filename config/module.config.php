b8510aa82b68f1226a616b9866c4a5ae050cc022
cristian <cristian@192.168.12.142>
2016-04-26 00:04:47 -0300

    Commit

--- config/module.config.php
+++ config/module.config.php
@@ -3,83 +3,28 @@
 return array(
     'view_manager' => array(
         'template_path_stack' => array(
-            'cdicalendar' => __DIR__ . '/../view',
+            'cdicmdb' => __DIR__ . '/../view',
         ),
     ),
     'controllers' => array(
         'invokables' => array(
-            'CdiCalendar\Controller\Calendar' => 'CdiCalendar\Controller\CalendarController',
-            'cdiagenda' => 'CdiCalendar\Controller\AgendaController',
+            'CdiCmdb\Controller\Adm' => 'CdiCmdb\Controller\AdmController',
+            'CdiCmdb\Controller\Main' => 'CdiCmdb\Controller\MainController',
         ),
     ),
     'router' => array(
         'routes' => array(
-            'cdiagenda' => array(
-                'type' => 'Literal',
-                'priority' => 1000,
-                'options' => array(
-                    'route' => '/cdiagenda',
-                    'defaults' => array(
-                        'controller' => 'agenda',
-                        'action' => 'index',
-                    ),
-                ),
-                'child_routes' => array(
-                    'list' => array(
-                        'type' => 'Segment',
-                        'options' => array(
-                            'route' => '/list[/:p]',
-                            'defaults' => array(
-                                'controller' => 'zfcuseradmin',
-                                'action' => 'list',
-                            ),
-                        ),
-                    ),
-                    'create' => array(
-                        'type' => 'Literal',
-                        'options' => array(
-                            'route' => '/create',
-                            'defaults' => array(
-                                'controller' => 'zfcuseradmin',
-                                'action' => 'create'
-                            ),
-                        ),
-                    ),
-                    'edit' => array(
-                        'type' => 'Segment',
-                        'options' => array(
-                            'route' => '/edit/:calendarId',
-                            'defaults' => array(
-                                'controller' => 'zfcuseradmin',
-                                'action' => 'edit',
-                                'userId' => 0
-                            ),
-                        ),
-                    ),
-                    'remove' => array(
-                        'type' => 'Segment',
-                        'options' => array(
-                            'route' => '/remove/:calendarId',
-                            'defaults' => array(
-                                'controller' => 'zfcuseradmin',
-                                'action' => 'remove',
-                                'userId' => 0
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-            'cdicalendar' => array(
+            'cdicmdb' => array(
                 'type' => 'segment',
                 'options' => array(
-                    'route' => '/cdicalendar[/:action][/:id]',
+                    'route' => '/cdicmdb/adm/[/:action][/:id]',
                     'constraints' => array(
                         'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
                         'id' => '[0-9]+',
                     ),
                     'defaults' => array(
-                        'controller' => 'CdiCalendar\Controller\Calendar',
-                        'action' => 'abm',
+                        'controller' => 'CdiCmdb\Controller\Adm',
+                        'action' => 'type',
                     ),
                 ),
             ),
@@ -87,18 +32,17 @@ return array(
     ),
     'doctrine' => array(
         'driver' => array(
-            'cdicalendar_entity' => array(
+            'cdicmdb_entity' => array(
                 'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
                 'paths' => __DIR__ . '/../src/CdiCalendar/Entity',
             ),
             'orm_default' => array(
                 'drivers' => array(
-                    'CdiCalendar\Entity' => 'cdicalendar_entity',
+                    'CdiCmdb\Entity' => 'cdicmdb_entity',
                 ),
             ),
         ),
     ),
-    'cdicalendar_options' => array(
-        'calendarEntityClass' => 'CdiCalendar/Entity/Calendar',
+    'cdicmdb_options' => array(
     )
 );
--- src/CdiCmdb/Controller/AdmController.php
+++ src/CdiCmdb/Controller/AdmController.php
@@ -4,9 +4,7 @@ namespace CdiCmdb\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 
-
 class AdmController extends AbstractActionController {
-
 
     /**
      * @var Doctrine\ORM\EntityManager
@@ -23,9 +21,8 @@ class AdmController extends AbstractActionController {
         }
         return $this->em;
     }
-    
-    
-     public function typeAction() {
+
+    public function typeAction() {
 
         $grid = $this->getServiceLocator()->get('cdiGrid');
         $source = new \CdiDataGrid\DataGrid\Source\Doctrine($this->getEntityManager(), '\CdiCmdb\Entity\CiType');
@@ -39,21 +36,94 @@ class AdmController extends AbstractActionController {
         $grid->hiddenColumn('createdBy');
         $grid->hiddenColumn('lastUpdatedBy');
 
-         $grid->addExtraColumn("<i class='fa fa-commenting-o ' ></i>", "<a class='btn btn-warning fa fa-commenting-o' href='/cdicmdb/adm/etype/{{id}}'></a>","left", false);
+        $grid->addExtraColumn("<i class='fa fa-commenting-o ' ></i>", "<a class='btn btn-warning fa fa-commenting-o' href='/cdicmdb/adm/etype/{{id}}'></a>", "left", false);
         $grid->addEditOption("Edit", "left", "btn btn-success fa fa-edit");
         //$grid->addDelOption("Del", "left", "btn btn-warning fa fa-trash");
-       $grid->addNewOption("Add", "btn btn-primary fa fa-plus", " Agregar");
+        $grid->addNewOption("Add", "btn btn-primary fa fa-plus", " Agregar");
         $grid->setTableClass("table-condensed customClass");
 
         $grid->prepare();
         return array('grid' => $grid);
     }
-    
-       public function etypeAction() {
-           
-           //tiene que verificar si existe la entidad, hacer reflection en tal caso, cargar las propiedades
-           
-       }
 
+    public function etypeAction() {
+        $id = $this->params("id");
+        //tiene que verificar si existe la entidad, hacer reflection en tal caso, cargar las propiedades
+        //1.Harcodeo el namespace y path, a fin decuenas tiene que quedar en este vendor
+
+        $namespace = "CdiCmdb\Entity";
+        $path = __DIR__ . "/Entity";
+
+
+        $query = $this->getEntityManager()->createQueryBuilder()
+                ->select('u')
+                ->from('CdiCmdb\Entity\CiType', 'u')
+                ->where("u.id = :id")
+                ->setParameter("id", $id);
+        $ciType = $query->getQuery()->getOneOrNullResult();
+
+        if ($ciType) {
+            $query = $this->getEntityManager()->createQueryBuilder()
+                    ->select('u')
+                    ->from('CdiEntity\Entity\Entity', 'u')
+                    ->where("u.id = :id")
+                    ->setParameter("id", $ciType->getName());
+            $entity = $query->getQuery()->getOneOrNullResult();
+        }
+
+        if (!$entity) {
+
+            $namespaces = new \CdiEntity\Entity\Namespaces();
+            $namespaces->setName($namespace);
+            $namespaces->setPath($path);
+
+            $entity = new \CdiEntity\Entity\Entity();
+            $entity->setName($namespace . $ciType->getName());
+            $entity->setPath($path);
+            $entity->setNamespace($namespaces);
+
+            $this->getEntityManager()->persist($namespaces);
+            $this->getEntityManager()->persist($entity);
+            $this->getEntityManager()->flush();
+        }
+
+
+        $query = $this->getEntityManager()->createQueryBuilder()
+                ->select('u')
+                ->from('CdiEntity\Entity\Property', 'u')
+                ->where("u.entity = :id")
+                ->setParameter("id", $entity);
+
+
+
+
+        $grid = $this->getServiceLocator()->get('cdiGrid');
+        $source = new \CdiDataGrid\DataGrid\Source\Doctrine($this->getEntityManager(), '\CdiEntity\Entity\Property', $query);
+        $grid->setSource($source);
+        $grid->setRecordPerPage(20);
+        $grid->datetimeColumn('createdAt', 'Y-m-d H:i:s');
+        $grid->datetimeColumn('updatedAt', 'Y-m-d H:i:s');
+        $grid->datetimeColumn('expiration', 'Y-m-d H:i:s');
+        $grid->hiddenColumn('createdAt');
+        $grid->hiddenColumn('updatedAt');
+        $grid->hiddenColumn('createdBy');
+        $grid->hiddenColumn('lastUpdatedBy');
+
+        $grid->addExtraColumn("<i class='fa fa-commenting-o ' ></i>", "<a class='btn btn-warning fa fa-commenting-o' href='/cdicmdb/adm/etype/{{id}}'></a>", "left", false);
+        $grid->addEditOption("Edit", "left", "btn btn-success fa fa-edit");
+        //$grid->addDelOption("Del", "left", "btn btn-warning fa fa-trash");
+        $grid->addNewOption("Add", "btn btn-primary fa fa-plus", " Agregar");
+        $grid->setTableClass("table-condensed customClass");
+
+        $grid->prepare();
+
+
+        if ($this->request->getPost("crudAction") == "edit" || $this->request->getPost("crudAction") == "add") {
+            $grid->getEntityForm()->get("entity")->setValue($entity->getId());
+       
+        }
+
+        return array('grid' => $grid);
+    }
 
 }
